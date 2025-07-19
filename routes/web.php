0dddd5ea39086cc517ad000e460977e414963a8c
Caxton Muthoni <githinjicaxton323@gmail.com>
2025-07-19 20:25:44 +0300

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render(
+        'Welcome'
+    );
 });
 
 Route::get('/dashboard', function () {
@@ -24,4 +21,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
